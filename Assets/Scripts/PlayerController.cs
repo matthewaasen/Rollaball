d6f4d6bcc2a158de063d020e7d0b67fa886a984c
matthewaasen <112990586+matthewaasen@users.noreply.github.com>
2026-01-29 16:20:18 -0600

    made moving player ball and camera

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+    public float speed = 0;
+
+    void Start()
+    {
+        rb = GetComponent <Rigidbody>(); 
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+    void OnMove (InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>(); 
+        movementX = movementVector.x; 
+        movementY = movementVector.y; 
+        
+
+    }
+}
