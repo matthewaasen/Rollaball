feb14875741bac4b585f496744fffc5e5364b870
matthewaasen <112990586+matthewaasen@users.noreply.github.com>
2026-02-01 19:24:27 -0600

    point count and win condition

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,33 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private Rigidbody rb;
+    private int count = 0;
     private float movementX;
     private float movementY;
 
     public float speed = 0;
 
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 8)
+       {
+           winTextObject.SetActive(true);
+       }
+
+    }
     void Start()
     {
+        winTextObject.SetActive(false);
         rb = GetComponent <Rigidbody>(); 
+        SetCountText();
     }
 
     private void FixedUpdate()
@@ -31,9 +46,12 @@ public class PlayerController : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
+ 
         if(other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);  
+            count = count + 1;
+            SetCountText();
         }
         
     } 
