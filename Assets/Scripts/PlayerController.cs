11b627380ab6e0255732f63426dbc1b0a0fdbe70
matthewaasen <112990586+matthewaasen@users.noreply.github.com>
2026-02-04 00:36:25 -0600

    added enemy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,6 +20,7 @@ public class PlayerController : MonoBehaviour
         if (count >= 8)
        {
            winTextObject.SetActive(true);
+           Destroy(GameObject.FindGameObjectWithTag("Enemy"));
        }
 
     }
@@ -55,4 +56,16 @@ public class PlayerController : MonoBehaviour
         }
         
     } 
+
+    private void OnCollisionEnter(Collision collision)
+    {
+    if (collision.gameObject.CompareTag("Enemy"))
+    {
+        // Destroy the current object
+        Destroy(gameObject); 
+        // Update the winText to display "You Lose!"
+        winTextObject.gameObject.SetActive(true);
+        winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+    }
+    }
 }
